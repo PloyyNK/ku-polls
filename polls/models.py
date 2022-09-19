5d57249de6c2f8ee4a5093cd5d4cf9a79e2761f4
nartnattakrivichian <nartnatta_kri@yahoo.com>
2022-09-20 05:54:58 +0700

    add Vote

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -40,8 +41,21 @@ class Choice(models.Model):
     """Text of choice"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+
+    @property
+    def votes(self):
+        """Total vote"""
+        return Vote.objects.filter(choice=self).count()
 
     def __str__(self):
         """Return text of choice"""
         return self.choice_text
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    @property
+    def __str__(self):
+        return f"user={self.user}, question={self.choice.question}, choice={self.choice.choice_text}"
