f22b34f615eddb24824a18e41efb35a0025ec2f5
nartnattakrivichian <nartnatta_kri@yahoo.com>
2022-10-16 14:54:44 +0700

    fix default

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", cast=str, default="django-insecure-g^2kk+sb%o^pkw_o7ika0lxzht$f5jb9a1*)$94q!%ct$=e4gf")
+SECRET_KEY = config("SECRET_KEY", cast=str, default="missing")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=True)
